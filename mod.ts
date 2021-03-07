7efa8737ee01fa953ee00618f868c25bdc7e5913
Josh <josh@grift.ca>
2021-03-07 18:55:55 -0500

    Moved to nodejs

--- mod.ts
+++ mod.ts
@@ -1,29 +1,24 @@
-import {
-  Application,
-  send,
-  Router,
-} from "https://deno.land/x/oak@v6.5.0/mod.ts";
+import * as Express from "express";
+import * as bodyParser from "body-parser";
+import * as path from "path";
+import fetch from "node-fetch";
 
-const app = new Application();
+const app = Express();
 
-const router = new Router();
+app.use(bodyParser.json());
 
-router.get("/", async (ctx) => {
-  await send(ctx, "index.html", { root: `${Deno.cwd()}/public` });
+app.get("/", (req: Express.Request, res: Express.Response) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
 
-router.post("/request", async (ctx) => {
-  const { value } = ctx.request.body({ type: "json" });
-  const { url } = await value;
+app.post("/request", async (req: Express.Request, res: Express.Response) => {
+  var result = await fetch(req.body.url);
 
-  var result = await fetch(url);
-
-  ctx.response.body = await result.json();
+  res.json(await result.json());
 });
 
-app.use(router.routes());
-app.use(async (ctx) => {
-  await send(ctx, ctx.request.url.pathname, { root: `${Deno.cwd()}/public` });
-});
+app.use(
+  Express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+);
 
-await app.listen("127.0.0.1:3000");
+app.listen(3000);
